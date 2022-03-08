b588bd2c9194e4d2fe0ffad13dbffbc79e777754
lindros <lindrosss88@yandex.ru>
2022-03-09 01:18:04 +0300

    Ограничение прав доступа к действиям компаний

--- crm/controllers/CompaniesController.php
+++ crm/controllers/CompaniesController.php
@@ -8,19 +8,24 @@ use app\models\CompaniesSearch;
 use app\models\ContactsSearch;
 use app\models\Contacts;
 use app\models\Projects;
+use app\models\Tasks;
 use app\models\ProjectsSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+
+use yii\filters\AccessControl;
 
 /**
  * CompaniesController implements the CRUD actions for Companies model.
  */
 class CompaniesController extends Controller
 {
+    public $enableCsrfValidation = false;
     /**
      * {@inheritdoc}
      */
+
     public function behaviors()
     {
         return [
@@ -30,8 +35,38 @@ class CompaniesController extends Controller
                     'delete' => ['POST'],
                 ],
             ],
+
+            'access' => [
+                'class' => AccessControl::class,
+               // 'only' => ['login', 'logout', 'signup'],
+                'denyCallback' => function () {
+                    die('Доступ запрещен!');
+                },
+
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['login', 'signup'],
+                        'roles' => ['?'],
+                    ],
+
+                    [
+                        'allow' => false,
+                        'actions' => ['index', 'update', 'view', 'viewcompany'],
+                        'roles' => ['?'],
+                    ],
+
+                    [
+                        'allow' => true,
+                        //'actions' => ['logout', 'index', /*'update'*/],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+
         ];
     }
+
 
     /**
      * Lists all Companies models.
@@ -120,6 +155,91 @@ class CompaniesController extends Controller
             'searchModelProjects' => $searchModelProjects,
             'dataProviderProjects' => $dataProviderProjects,
         ]);
+    }
+
+    public function actionViewcompany($id)
+    {
+        //echo var_dump(Yii::$app->request->post());
+        $company = $this->findModel($id);
+
+        $projects = $company->getCompany_projects();
+        $contacts = $company->getCompany_contacts();
+
+        /*
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['index', []]);
+        }*/
+
+        /**/
+        $searchModelContacts = new ContactsSearch();
+        $dataProviderContacts = $searchModelContacts->search(Yii::$app->request->queryParams, $id );
+
+        $searchModelProjects = new ProjectsSearch();
+        $dataProviderProjects = $searchModelProjects->search(Yii::$app->request->queryParams, $id );
+
+        /**/
+
+        return $this->render('veiw_company', [
+            'model' => $company,
+            'company' => $company,
+            'projects' => $projects,
+            'contacts' => $contacts,
+
+            'searchModelContacts' => $searchModelContacts,
+            'dataProviderContacts' => $dataProviderContacts,
+
+            'searchModelProjects' => $searchModelProjects,
+            'dataProviderProjects' => $dataProviderProjects,
+        ]);
+    }
+
+    public function actionGet_contacts_part(){
+        $post = Yii::$app->request->post();
+
+        if(isset($post['company_id'])){
+            $company = $this->findModel($post['company_id']);
+            $contacts = $company->getCompany_contacts();
+
+            return \Yii::$app->view->renderFile('@app/views/companies/part_contacts.php', [
+                'contacts' => $contacts,
+                ]);
+        }else{
+            return '<p>Ошибка отображения контактов</p>';
+        }
+    }
+
+    public function actionGet_projects_part(){
+        $post = Yii::$app->request->post();
+
+        if(isset($post['company_id'])){
+            $company = $this->findModel($post['company_id']);
+            $projects = $company->getCompany_projects();
+
+            return \Yii::$app->view->renderFile('@app/views/companies/part_projects.php', [
+                'projects' => $projects,
+            ]);
+        }else{
+            return '<p>Ошибка отображения контактов</p>';
+        }
+    }
+
+    public function actionGet_tasks_part(){
+        //return '-';
+        $post = Yii::$app->request->post();
+
+        if(isset($post['id_project'])){
+            //$company = $this->findModel($post['id_project']);
+            $tasks = Tasks::find()
+                ->where(['id_project' =>$post['id_project']])
+                ->all();
+
+
+            return \Yii::$app->view->renderFile('@app/views/companies/part_tasks.php', [
+                'tasks' => $tasks,
+            ]);
+        }else{
+            return '-';
+        }
     }
 
     /**
